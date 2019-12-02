73040bc76f8e057a3ac2d031fb4c3e1f615118da
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2019-12-02 13:19:42 +0800

    Add comments for 2015 day 2

--- 2015/day2/2015-day2.js
+++ 2015/day2/2015-day2.js
@@ -2,19 +2,43 @@ const fs = require('fs');
 const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\n');
 
-const parseDimensions = line => line.split('x').map(d => parseInt(d)).sort((a, b) => a - b);
+/**
+ * Functional pipe operator used to compose functions together
+ * @param  {...any} fns - functions to compose, applied left to right
+ */
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
 
-const calculateWrappingPaper = line => (sortedDimensions => 
-  sortedDimensions[0]*sortedDimensions[1]*2 +
-  sortedDimensions[1]*sortedDimensions[2]*2 +
-  sortedDimensions[0]*sortedDimensions[2]*2 +
-  sortedDimensions[0]*sortedDimensions[1]
-)(parseDimensions(line))
+/**
+ * Parses dimensions from a string into a tuple [A,B,C]
+ * @param {string} line - dimensions in AxBxC format
+ */
+const parseDimensions = line =>
+  line
+    .split('x')
+    .map(d => parseInt(d))
+    .sort((a, b) => a - b);
 
-const calculateRibbonLength = line => (sortedDimensions => 
-  (sortedDimensions[0]+sortedDimensions[1])*2 +
-  sortedDimensions[0]*sortedDimensions[1]*sortedDimensions[2]
-)(parseDimensions(line))
+/**
+ * Calculates the wrapping paper area required for a present of dimensions AxBxC
+ * @param {string} line - dimensions in AxBxC format
+ */
+const calculateWrappingPaper = pipe(
+  parseDimensions,
+  dimensions =>
+    dimensions[0] * dimensions[1] * 2 +
+    dimensions[1] * dimensions[2] * 2 +
+    dimensions[0] * dimensions[2] * 2 +
+    dimensions[0] * dimensions[1]
+);
+
+/**
+ * Calculates the ribbon length required for a present of dimensions AxBxC
+ * @param {string} line - dimensions in AxBxC format
+ */
+const calculateRibbonLength = pipe(
+  parseDimensions,
+  dimensions => (dimensions[0] + dimensions[1]) * 2 + dimensions[0] * dimensions[1] * dimensions[2]
+);
 
 const answer1 = input.map(calculateWrappingPaper).reduce((a, x) => a + x);
 console.log('2015 Day 2 Part 1:', answer1);
@@ -27,4 +51,4 @@ module.exports = {
   calculateRibbonLength,
   answer1,
   answer2
-}
+};
