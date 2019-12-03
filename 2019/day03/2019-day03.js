93cdc35639dd7b75349d6a92b72e91708a138e23
Hung-Yi Loo <hungyi.loo@wintermornings.net>
2019-12-03 16:59:03 +0800

    Refactor 2019 Day 3 with comments

--- 2019/day03/2019-day03.js
+++ 2019/day03/2019-day03.js
@@ -3,63 +3,131 @@ const path = require('path');
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\n');
 
 /**
+ * Functional pipe operator used to compose functions together
+ * @param  {...any} fns - functions to compose, applied left to right
+ */
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+/**
  * Generate an array with values from 0 up to (but not including) the specified size
  * @param {number} size - range to generate
  */
 const range = size => Array(size).fill().map((_, i) => i);
 
+/**
+ * Increment the given number
+ * @param {number} x 
+ */
 const increment = x => x + 1;
 
-const tracePath = directions => directions
-  .split(',')
-  .reduce((path, movement) => {
-    const [direction, ...distanceChars] = movement.split('');
-    const distance = parseInt(distanceChars.join(''));
-    const [lastX, lastY] = path[path.length - 1];
-    switch(direction) {
-      case 'R': return [...path, ...range(distance).map(increment).map(delta => [lastX + delta, lastY])];
-      case 'L': return [...path, ...range(distance).map(increment).map(delta => [lastX - delta, lastY])];
-      case 'U': return [...path, ...range(distance).map(increment).map(delta => [lastX, lastY + delta])];
-      case 'D': return [...path, ...range(distance).map(increment).map(delta => [lastX, lastY - delta])];
-    }
-  }, [[0,0]])
+/**
+ * Get the destination coordinate after moving in the specified direction and distance from a known point
+ * @param {string} direction - the direction to move toward
+ * @param {[number, number]} fromPoint - the point to move from
+ * @param {number} distance - the number of steps to take 
+ */
+const move = (direction, [fromX, fromY]) => distance => {
+  switch (direction) {
+    case 'R': return [fromX + distance, fromY];
+    case 'L': return [fromX - distance, fromY];
+    case 'U': return [fromX, fromY + distance];
+    case 'D': return [fromX, fromY - distance];
+  }
+};
 
+/**
+ * Trace a path and output all of its coordinates, step by step
+ * @param {string} directions - comma separated instructions in the puzzle format, e.g. U7,R6,D4,L4...
+ */
+const tracePath = directions =>
+  directions.split(',').reduce(
+    (path, vector) => {
+      const [direction, ...distanceChars] = vector.split('');
+      const distance = parseInt(distanceChars.join(''));
+      const lastPosition = path[path.length - 1];
+      return [
+        ...path,
+        ...range(distance)
+          .map(increment)
+          .map(move(direction, lastPosition))
+      ];
+    },
+    [[0, 0]]
+  );
+
+/**
+ * Return a set of all coordinates in a path (unique by their JSON representation)
+ * @param {[number,number][]} path 
+ */
 const getCoordinateSet = path => path.reduce((s, point) => s.add(JSON.stringify(point)), new Set());
 
-const intersection = (set1, set2) => Array.from(set1.values())
-  .reduce((result, curr) => set2.has(curr) ? result.concat(curr) : result, []);
+/**
+ * Find the intersection between two sets
+ * @param {Set} set1 
+ * @param {Set} set2 
+ */
+const binaryIntersection = (set1, set2) =>
+  Array.from(set1.values()).reduce((result, curr) => (set2.has(curr) ? result.add(curr) : result), new Set());
 
-const findCrossingPoints = (path1, path2) => intersection(
-  getCoordinateSet(path1),
-  getCoordinateSet(path2)
-).map(pointJson => JSON.parse(pointJson));
+/**
+ * Find the intersection of all given sets
+ * @param  {...Set[]} sets - sets to intersect
+ */
+const intersection = (...sets) => sets.reduce((result, currSet) => binaryIntersection(result, currSet));
 
-const manhattanDistanceFromOrigin = (...paths) => point => Math.abs(point[0]) + Math.abs(point[1]);
+/**
+ * Find all coordinates where the given paths intersect
+ * @param  {...[number,number][]} paths
+ */
+const findCrossingPoints = (...paths) =>
+  pipe(
+    paths => paths.map(getCoordinateSet),
+    pathCoordinateSets => intersection(...pathCoordinateSets),
+    commonCoordinatesSet => Array.from(commonCoordinatesSet.values()),
+    commonCoordinateJsonValues => commonCoordinateJsonValues.map(c => JSON.parse(c))
+  )(paths);
 
-const timingDistance = (...paths) => 
-  point => paths
-    .map(path => path.findIndex(p => p[0] === point[0] && p[1] === point[1]))
+/**
+ * Find the Manhattan Distance from the origin for a given coordinate
+ * @param  {...[number,number][]} paths
+ * @param  {[number,number]} coordinate
+ */
+const manhattanDistanceFromOrigin = (...paths) => ([x, y]) => Math.abs(x) + Math.abs(y);
+
+/**
+ * For a given point, find the total number of steps for all given paths to get to that point
+ * @param  {...[number,number][]} paths
+ * @param  {[number,number]} coordinate
+ */
+const combinedStepsDistance = (...paths) => ([x, y]) =>
+  paths
+    .map(path => path.findIndex(([pathX, pathY]) => pathX === x && pathY === y))
+    .filter(distance => distance !== -1)
     .reduce((a, b) => a + b);
 
-const shortestCrossingPointDistance = (distanceMeasure, directions1, directions2) => {
-  const path1 = tracePath(directions1);
-  const path2 = tracePath(directions2);
-  return findCrossingPoints(path1, path2)
-    .map(distanceMeasure(path1, path2))
-    .sort((a, b) => a - b)
-    [1]; // Ignore the origin
-}
+/**
+ * Find the closest crossing point for all given paths, measured by the given distance measure function
+ * @param {[number,number][] => [number,number] => number} distanceMeasure 
+ * @param  {...string[]} directions 
+ */
+const shortestCrossingPointDistance = (distanceMeasure, ...directions) =>
+  pipe(
+    directions => directions.map(tracePath),
+    paths => findCrossingPoints(...paths).map(distanceMeasure(...paths)),
+    crossingPointDistances => crossingPointDistances.sort((a, b) => a - b),
+    sortedDistances => sortedDistances[1] // Ignore the origin
+  )(directions);
 
-const answer1 = shortestCrossingPointDistance(manhattanDistanceFromOrigin, input[0], input[1]);
+const answer1 = shortestCrossingPointDistance(manhattanDistanceFromOrigin, ...input);
 console.log('2019 Day 3 Part 1:', answer1);
 
-const answer2 = shortestCrossingPointDistance(timingDistance, input[0], input[1]);
+const answer2 = shortestCrossingPointDistance(combinedStepsDistance, ...input);
 console.log('2019 Day 3 Part 2:', answer2);
 
 module.exports = {
   shortestCrossingPointDistance,
   manhattanDistanceFromOrigin,
-  timingDistance,
+  combinedStepsDistance,
   answer1,
   answer2
-}
+};
--- 2019/day03/2019-day03.test.js
+++ 2019/day03/2019-day03.test.js
@@ -1,7 +1,7 @@
 const {
   shortestCrossingPointDistance,
   manhattanDistanceFromOrigin,
-  timingDistance,
+  combinedStepsDistance,
   answer1,
   answer2
 } = require('./2019-day03');
@@ -36,20 +36,20 @@ test('2019 day 3 part 1 answer', () => {
 test('2019 day 3 part 2 examples', () => {
   expect(
     shortestCrossingPointDistance(
-      timingDistance, 
+      combinedStepsDistance, 
       'R8,U5,L5,D3', 
       'U7,R6,D4,L4'
     )).toBe(30);
   expect(
     shortestCrossingPointDistance(
-      timingDistance,
+      combinedStepsDistance,
       'R75,D30,R83,U83,L12,D49,R71,U7,L72',
       'U62,R66,U55,R34,D71,R55,D58,R83'
     )
   ).toBe(610);
   expect(
     shortestCrossingPointDistance(
-      timingDistance,
+      combinedStepsDistance,
       'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
       'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
     )
