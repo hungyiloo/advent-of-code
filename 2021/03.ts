2441df8b1d91ce636f4853bcbfea265842b2d164
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-03 15:59:00 +0800

    Simplify 2021 day 3 massively

--- 2021/03.ts
+++ 2021/03.ts
@@ -1,54 +1,32 @@
 import { range } from "../lib/array.ts";
-import { getLines, from, pop, reduce, toArray } from "../lib/streams.ts";
+import { from, getLines, pop, reduce, toArray } from "../lib/streams.ts";
 
-const data = () => getLines("03.input.txt")
+const data = () => getLines("03.input.txt");
 
-const bits = (await pop(data()))?.length ?? 0
+const bits = (await pop(data()))?.length ?? 0;
 
-const tallyBits = (numbers: ReturnType<typeof data>) =>
-  reduce(
+async function majorityBits(numbers: ReturnType<typeof data>, invert?: boolean) {
+  const scores = await reduce(
     numbers,
-    (acc, curr) =>
-      acc.map((tally, i) => ({
-        one: tally.one + (curr[i] === "1" ? 1 : 0),
-        zero: tally.zero + (curr[i] === "0" ? 1 : 0),
-      })),
-    range(bits).map(() => ({
-      one: 0,
-      zero: 0,
-    })),
+    (acc, curr) => acc.map((s, i) => s + (curr[i] === "1" ? 1 : -1)),
+    range(bits).map(() => 0),
   );
 
-function chooseBit(
-  tally: { one: number; zero: number },
-  invert?: boolean,
-) {
-  return (invert
-      ? tally.zero > tally.one
-      : tally.one >= tally.zero)
-    ? "1"
-    : "0";
+  return scores
+    .map((s) => (invert ? s < 0 : s >= 0) ? "1" : "0")
+    .join("");
 }
 
-const gamma = parseInt(
-  (await tallyBits(data()))
-    .map((x) => chooseBit(x))
-    .join(""),
-  2,
-);
-
+const gamma = parseInt(await majorityBits(data()), 2);
 const epsilon = ~gamma & parseInt("1".repeat(bits), 2);
-
 console.log("Part 1:", gamma * epsilon);
 
 async function search(invert?: boolean) {
   let filteredNumbers = await toArray(data());
-  let tallies = await tallyBits(from(filteredNumbers));
 
   for (const i of range(bits)) {
-    const common = chooseBit(tallies[i], invert);
-    filteredNumbers = filteredNumbers.filter((num) => num[i] === common);
-    tallies = await tallyBits(from(filteredNumbers));
+    const majority = await majorityBits(from(filteredNumbers), invert);
+    filteredNumbers = filteredNumbers.filter((num) => num[i] === majority[i]);
     if (filteredNumbers.length <= 1) break;
   }
 
@@ -58,5 +36,4 @@ async function search(invert?: boolean) {
 
 const o2GeneratorRating = await search();
 const co2ScrubberRating = await search(true);
-
 console.log("Part 2:", o2GeneratorRating * co2ScrubberRating);
