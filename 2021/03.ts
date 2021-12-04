362b5f7a1624a5ea02044fb5c2728f304103089d
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-04 18:38:51 +0800

    Don't like async fluff in day 3 --- get rid of it

--- 2021/03.ts
+++ 2021/03.ts
@@ -1,39 +1,34 @@
 import { range } from "../lib/array.ts";
-import { pipe } from "../lib/pipe.ts";
-import { from, getLines, pop, reduce, toArray } from "../lib/streams.ts";
+import { getLines, toArray } from "../lib/streams.ts";
 
-const data = () => getLines("03.input.txt");
+const data = await toArray(getLines("03.input.txt"));
+const bits = data[0]?.length ?? 0;
 
-const bits = (await pipe(data(), pop))?.length ?? 0;
-
-async function majorityBits(numbers: ReturnType<typeof data>, invert?: boolean) {
-  const scores = await pipe(
-    numbers,
-    reduce(
+function majorityBits(numbers: string[], invert?: boolean) {
+  return numbers
+    .reduce(
       (acc, curr) => acc.map((s, i) => s + (curr[i] === "1" ? 1 : -1)),
       range(bits).map(() => 0),
-    ),
-  );
-  return scores
+    )
     .map((s) => (invert ? s < 0 : s >= 0) ? "1" : "0")
     .join("");
 }
 
-const gamma = parseInt(await majorityBits(data()), 2);
+const gamma = parseInt(majorityBits(data), 2);
 const epsilon = ~gamma & parseInt("1".repeat(bits), 2); // bitwise inverse with mask
 console.log("Part 1:", gamma * epsilon);
 
 async function search(invert?: boolean) {
-  let filteredNumbers = await toArray(data());
+  const match = range(bits).reduce(
+    (acc, i) => {
+      if (acc.length <= 1) return acc
+      const majority = majorityBits(acc, invert);
+      return acc.filter((num) => num[i] === majority[i]);
+    },
+    data
+  )[0];
 
-  for (const i of range(bits)) {
-    const majority = await majorityBits(from(filteredNumbers), invert);
-    filteredNumbers = filteredNumbers.filter((num) => num[i] === majority[i]);
-    if (filteredNumbers.length <= 1) break;
-  }
-
-  const result = filteredNumbers[0];
-  return result ? parseInt(result, 2) : 0;
+  return parseInt(match, 2)
 }
 
 const o2GeneratorRating = await search();
