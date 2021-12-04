a66ad921c79cc8203d2bc9e78483ff8f38a1db9a
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-04 20:04:42 +0800

    Fix unnecessary async/await and refactor for brevity

--- 2021/03.ts
+++ 2021/03.ts
@@ -4,33 +4,32 @@ import { getLines, toArray } from "../lib/streams.ts";
 const data = await toArray(getLines("03.input.txt"));
 const bits = data[0]?.length ?? 0;
 
-function majorityBits(numbers: string[], invert?: boolean) {
-  return numbers
+const majority = (numbers: string[], invert?: boolean) =>
+  numbers
     .reduce(
       (acc, curr) => acc.map((s, i) => s + (curr[i] === "1" ? 1 : -1)),
       range(bits).map(() => 0),
     )
     .map((s) => (invert ? s < 0 : s >= 0) ? "1" : "0")
     .join("");
-}
 
-const gamma = parseInt(majorityBits(data), 2);
+const gamma = parseInt(majority(data), 2);
 const epsilon = ~gamma & parseInt("1".repeat(bits), 2); // bitwise inverse with mask
 console.log("Part 1:", gamma * epsilon);
 
-async function search(invert?: boolean) {
+function search(invert?: boolean) {
   const match = range(bits).reduce(
     (acc, i) => {
-      if (acc.length <= 1) return acc
-      const majority = majorityBits(acc, invert);
-      return acc.filter((num) => num[i] === majority[i]);
+      if (acc.length <= 1) return acc;
+      const m = majority(acc, invert);
+      return acc.filter((num) => num[i] === m[i]);
     },
-    data
+    data,
   )[0];
 
-  return parseInt(match, 2)
+  return parseInt(match, 2);
 }
 
-const o2GeneratorRating = await search();
-const co2ScrubberRating = await search(true);
+const o2GeneratorRating = search();
+const co2ScrubberRating = search(true);
 console.log("Part 2:", o2GeneratorRating * co2ScrubberRating);
