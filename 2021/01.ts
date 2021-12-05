9d057f67b8bd26d84bf045f456d92d2cb6e27be0
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-05 19:58:44 +0800

    Refactor day 01 to be more duplicated but more readable/explicit

--- 2021/01.ts
+++ 2021/01.ts
@@ -1,26 +1,20 @@
 import { pipe } from "../lib/pipe.ts";
 import { count, getLines, map, slidingWindow } from "../lib/streams.ts";
 
-const depths = () =>
-  pipe(
-    getLines("01.input.txt"),
-    map(Number),
-  );
-
-const upticks = (numbers$: ReturnType<typeof depths>) =>
-  pipe(
-    numbers$,
-    slidingWindow(2),
-    count(([x, y]) => x < y)
-  );
-
-const sinkCountPart1 = await upticks(depths());
+const sinkCountPart1 = await pipe(
+  getLines("01.input.txt"),
+  map(Number),
+  slidingWindow(2), // pairwise
+  count(([x, y]) => x < y), // count pairs that increase
+);
 console.log("Part 1:", sinkCountPart1);
 
 const sinkCountPart2 = await pipe(
-  depths(),
-  slidingWindow(3),
-  map((arr) => arr.reduce((a, x) => a + x)),
-  upticks,
+  getLines("01.input.txt"),
+  map(Number),
+  slidingWindow(3), // triplewise
+  map((arr) => arr.reduce((a, x) => a + x)), // sum each triple
+  slidingWindow(2), // pairwise
+  count(([x, y]) => x < y), // count pairs that increase
 );
 console.log("Part 2:", sinkCountPart2);
