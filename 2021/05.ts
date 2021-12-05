4bf01cadd959779af5de8e224fdbfdf4f09f47fd
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-05 18:18:48 +0800

    Use streaming partition on line type + clarity at end

--- 2021/05.ts
+++ 2021/05.ts
@@ -1,52 +1,55 @@
 import { range } from "../lib/array.ts";
 import { pipe } from "../lib/pipe.ts";
-import { getLines, map, toArray } from "../lib/streams.ts";
+import { getLines, map, reduce } from "../lib/streams.ts";
 
-type Point = [number, number];
+type Point = {
+  x: number;
+  y: number;
+};
 interface Line {
-  start: Point;
-  end: Point;
+  x1: number;
+  x2: number;
+  y1: number;
+  y2: number;
 }
 type Grid = Map<number, Map<number, number>>;
 
-const lines = await pipe(
+const isStraight = (l: Line) => l.x1 === l.x2 || l.y1 === l.y2;
+
+const [straightLines, diagonalLines] = await pipe(
   getLines("05.input.txt"),
   map((line) => line.split(" -> ")),
   map((points) => points.map((p) => p.split(",").map(Number))),
-  map(([start, end]) => ({ start, end } as Line)),
-  toArray,
+  map(([[x1, y1], [x2, y2]]) => ({ x1, x2, y1, y2 } as Line)),
+  reduce((acc, l) => {
+    // partition into straight and diagonal lines
+    acc[isStraight(l) ? 0 : 1].push(l)
+    return acc
+  }, [[],[]] as [Line[], Line[]]),
 );
 
-const isHorizontal = (line: Line) => line.start[1] === line.end[1];
-const isVertical = (line: Line) => line.start[0] === line.end[0];
-
-const makeGrid = (): Grid => new Map<number, Map<number, number>>();
-
 function plotPoint(grid: Grid, point: Point) {
-  const [x, y] = point;
+  const { x, y } = point;
   if (!grid.has(x)) grid.set(x, new Map<number, number>());
   const col = grid.get(x)!;
   col.set(y, (col.get(y) ?? 0) + 1);
 }
 
 function plotLine(grid: Grid, line: Line) {
-  const xs = range(line.start[0], line.end[0], true);
-  const ys = range(line.start[1], line.end[1], true);
+  const xs = range(line.x1, line.x2, true);
+  const ys = range(line.y1, line.y2, true);
   // iterate through the longer sequence of x or y values
   // (the other one MUST be a single value, as part of AoC conditions)
   range(Math.max(xs.length, ys.length))
     .forEach((i) =>
-      plotPoint(grid, [
-        xs.length === 1 ? xs[0] : xs[i],
-        ys.length === 1 ? ys[0] : ys[i],
-      ])
+      plotPoint(grid, {
+        x: xs.length === 1 ? xs[0] : xs[i],
+        y: ys.length === 1 ? ys[0] : ys[i],
+      })
     );
 }
 
-function plotLines(grid: Grid, lines: Line[], diagonals?: boolean) {
-  if (!diagonals) {
-    lines = lines.filter((line) => isHorizontal(line) || isVertical(line));
-  }
+function plotLines(grid: Grid, lines: Line[]) {
   lines.forEach((line) => plotLine(grid, line));
 }
 
@@ -56,10 +59,9 @@ function scoreGrid(grid: Grid) {
     .reduce((acc, curr) => acc + (curr > 1 ? 1 : 0), 0);
 }
 
-const part1Grid = makeGrid();
-plotLines(part1Grid, lines);
-console.log("Part 1:", scoreGrid(part1Grid));
+const grid = new Map<number, Map<number, number>>();
+plotLines(grid, straightLines);
+console.log("Part 1:", scoreGrid(grid));
 
-const part2Grid = makeGrid();
-plotLines(part2Grid, lines, true);
-console.log("Part 2:", scoreGrid(part2Grid));
+plotLines(grid, diagonalLines);
+console.log("Part 2:", scoreGrid(grid));
