a127355ec186fd35076da2be9d936b8fba6b0f59
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-05 18:22:59 +0800

    Clearer partitioning reduction

--- 2021/05.ts
+++ 2021/05.ts
@@ -16,16 +16,16 @@ type Grid = Map<number, Map<number, number>>;
 
 const isStraight = (l: Line) => l.x1 === l.x2 || l.y1 === l.y2;
 
-const [straightLines, diagonalLines] = await pipe(
+const { straightLines, diagonalLines } = await pipe(
   getLines("05.input.txt"),
   map((line) => line.split(" -> ")),
   map((points) => points.map((p) => p.split(",").map(Number))),
   map(([[x1, y1], [x2, y2]]) => ({ x1, x2, y1, y2 } as Line)),
   reduce((acc, l) => {
-    // partition into straight and diagonal lines
-    acc[isStraight(l) ? 0 : 1].push(l)
-    return acc
-  }, [[],[]] as [Line[], Line[]]),
+    if (isStraight(l)) acc.straightLines.push(l);
+    else acc.diagonalLines.push(l);
+    return acc;
+  }, { straightLines: [] as Line[], diagonalLines: [] as Line[] }),
 );
 
 function plotPoint(grid: Grid, point: Point) {
