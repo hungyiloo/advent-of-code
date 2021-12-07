3a365e3a3dd87ad579e1fe45b7b9a5030939fc8c
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-07 10:43:21 +0800

    More operation typing for pipe

--- lib/pipe.ts
+++ lib/pipe.ts
@@ -40,6 +40,28 @@ export function pipe<T, A, B, C, D, E, F>(
   op5: (x: D) => E,
   op6: (x: E) => F,
 ): F;
+export function pipe<T, A, B, C, D, E, F, G>(
+  input: T,
+  op1: (x: T) => A,
+  op2: (x: A) => B,
+  op3: (x: B) => C,
+  op4: (x: C) => D,
+  op5: (x: D) => E,
+  op6: (x: E) => F,
+  op7: (x: F) => G,
+): G;
+export function pipe<T, A, B, C, D, E, F, G, H>(
+  input: T,
+  op1: (x: T) => A,
+  op2: (x: A) => B,
+  op3: (x: B) => C,
+  op4: (x: C) => D,
+  op5: (x: D) => E,
+  op6: (x: E) => F,
+  op7: (x: F) => G,
+  op8: (x: G) => H,
+): H;
+// deno-lint-ignore no-explicit-any
 export function pipe<T>(input: T, ...ops: ((x: any) => any)[]) {
   return ops?.reduce((result, op) => op(result), input) ?? input;
 }
