2d4a7b8e2369d7c7b8cc7891a716c6eef791c966
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-11 12:09:14 +0800

    More terse f# day 3

--- 2021/03.fsx
+++ 2021/03.fsx
@@ -2,32 +2,30 @@ open System.IO
 open System
 
 let data =
-    File.ReadAllLines "03.input.txt" |> Seq.toList
+    File.ReadAllLines "03.input.txt" |> Seq.toList |> Seq.map Seq.toArray
 
-let bits = data |> Seq.head |> String.length
+let bits = data |> Seq.head |> Array.length
 
-type Combinator = string seq -> string
+type Combinator = char array seq -> char array
 
 let majority: Combinator =
     Seq.map (fun s -> Array.map (function '1' -> 1 | _ -> -1) (Seq.toArray s))
     >> Seq.reduce (fun acc curr -> Array.map2 (+) acc curr)
     >> Array.map (fun s -> if s >= 0 then '1' else '0')
-    >> String
 
 let minority: Combinator =
     majority
     >> Seq.toArray
     >> Array.map (function '0' -> '1' | _ -> '0')
-    >> String
 
-let binaryToInt (s: string) = Convert.ToInt32(s, 2)
+let binCharsToInt (s: char array) = Convert.ToInt32(String s, 2)
 
 [majority data; minority data]
-|> Seq.map binaryToInt
+|> Seq.map binCharsToInt
 |> Seq.reduce (*)
 |> printfn "Part 1: %A"
 
-let rec search (remaining: string seq) (bit: int) (combinator: Combinator) =
+let rec search (remaining: char array seq) (bit: int) (combinator: Combinator) =
     if (Seq.length remaining) <= 1 || bit = bits
     then remaining
     else
@@ -35,7 +33,7 @@ let rec search (remaining: string seq) (bit: int) (combinator: Combinator) =
         search (Seq.filter (fun s -> s[bit] = sieve) remaining) (bit + 1) combinator
 
 let startSearch (combinator: Combinator) =
-    (search data 0 combinator) |> Seq.head |> binaryToInt
+    (search data 0 combinator) |> Seq.head |> binCharsToInt
 
 [startSearch majority; startSearch minority]
 |> Seq.reduce (*)
