a06245379ad9274ca773e2996363c98033acea07
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-12 20:12:31 +0800

    Optimize day 12 f#

--- 2021/12.fsx
+++ 2021/12.fsx
@@ -1,5 +1,6 @@
 open System.IO
 open System
+open System.Collections.Generic
 
 type Cave =
     | Start
@@ -22,24 +23,33 @@ let parseLink (line: string) =
 
 let links = File.ReadAllLines "12.input.txt" |> Seq.map parseLink
 
+let memoize fn =
+    let cache = new Dictionary<_,_>()
+    (fun x ->
+        match cache.TryGetValue x with
+        | true, v -> v
+        | false, _ ->
+            let v = fn x
+            cache.Add(x, v)
+            v)
+
+let getAdjacentCaves =
+    memoize (fun n ->
+        links
+        |> Seq.fold
+            (fun acc link ->
+                match link with
+                | a, b when a = n -> b::acc
+                | a, b when b = n -> a::acc
+                | _ -> acc)
+            [])
+
 let rec walk pathFinder walked  =
     match walked with
     | [] -> walk pathFinder [Start]
     | End::_ -> 1 // return [walked] for tracing
     | cave::_ ->
-        let next =
-            links
-            |> Seq.fold
-                (fun acc link ->
-                    let maybeWalk n =
-                        match pathFinder walked n with
-                        | Some n -> n :: acc
-                        | None -> acc
-                    match link with
-                    | a, b when a = cave -> maybeWalk b
-                    | a, b when b = cave -> maybeWalk a
-                    | _ -> acc)
-                []
+        let next = getAdjacentCaves cave |> List.filter (pathFinder walked)
 
         if Seq.isEmpty next
         then 0 // return [] for tracing
@@ -50,22 +60,21 @@ let rec walk pathFinder walked  =
 
 let pathfinder1 walked n =
     match n with
-    | Small _ when not (Seq.contains n walked) -> Some n
-    | Big _ | End -> Some n
-    | _ -> None
+    | Small _ when not (Seq.contains n walked) -> true
+    | Big _ | End -> true
+    | _ -> false
 
-let canVisitTwice walked =
-    walked
-    |> Seq.choose (function | Small n -> Some n | _ -> None)
-    |> Seq.countBy id
-    |> Seq.exists (fun (_, count) -> count >= 2)
-    |> not
+let canStillVisitTwice  =
+    Seq.choose (function | Small n -> Some n | _ -> None)
+    >> Seq.countBy id
+    >> Seq.exists (fun (_, count) -> count >= 2)
+    >> not
 
 let pathfinder2 walked n =
     match n with
-    | Small _ when (canVisitTwice walked) || not (Seq.contains n walked) -> Some n
-    | Big _ | End -> Some n
-    | _ -> None
+    | Small _ when (canStillVisitTwice walked) || not (Seq.contains n walked) -> true
+    | Big _ | End -> true
+    | _ -> false
 
 printfn "Part 1: %A" (walk pathfinder1 [])
 printfn "Part 2: %A" (walk pathfinder2 [])
