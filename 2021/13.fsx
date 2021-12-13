fc0668df6455f3a1c5015b6cf381213b413f2031
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-13 21:02:27 +0800

    Day 13 minor optimizations and refactoring for fun

--- 2021/13.fsx
+++ 2021/13.fsx
@@ -1,28 +1,29 @@
 open System.IO
 open System
 
-type Line =
+type Fold =
     | Vertical of int
     | Horizontal of int
 
-let lines = File.ReadAllLines "13.input.txt"
+let (|SplitLine|_|) (separator: char) (line: string) =
+    match line.Split(separator) with
+    | [| left; right |] -> Some (left, right)
+    | _ -> None
 
-let dots =
-    lines
-    |> Seq.choose
-        (fun line ->
-            match line.Split(',') with
-            | [| a; b |] -> Some(int a, int b)
-            | _ -> None)
-
-let folds =
-    lines
-    |> Seq.choose
-        (fun line ->
-            match line.Split('=') with
-            | [| "fold along x"; a |] -> a |> int |> Vertical |> Some
-            | [| "fold along y"; a |] -> a |> int |> Horizontal |> Some
-            | _ -> None)
+let dots, folds =
+    File.ReadLines "13.input.txt"
+    |> Seq.fold
+        (fun (dots, folds) line ->
+            match line with
+            | SplitLine ',' (x, y) -> (int x, int y)::dots, folds
+            | SplitLine '=' (direction, position) ->
+                match direction with
+                | "fold along x" -> dots, Vertical (int position) :: folds
+                | "fold along y" -> dots, Horizontal (int position) :: folds
+                | _ -> dots,folds
+            | _ -> dots, folds)
+        ([], [])
+    |> (fun (dots, folds) -> dots, folds |> List.rev)
 
 let origami dots folds =
     folds
@@ -30,17 +31,18 @@ let origami dots folds =
         (fun acc curr ->
             let foldOperation =
                 match curr with
-                 | Horizontal dividerY ->
-                    (fun (x, y) -> x, (if y > dividerY then 2*dividerY - y else y))
-                 | Vertical dividerX ->
-                    (fun (x, y) -> (if x > dividerX then 2*dividerX - x else x), y)
+                 | Horizontal lineAtY -> (fun (x, y) -> x, (if y > lineAtY then 2*lineAtY - y else y))
+                 | Vertical lineAtX -> (fun (x, y) -> (if x > lineAtX then 2*lineAtX - x else x), y)
             Seq.map foldOperation acc)
         dots
     |> Seq.distinct
 
 let plot dots =
-    let maxX = dots |> Seq.map fst |> Seq.max
-    let maxY = dots |> Seq.map snd |> Seq.max
+    let maxX, maxY =
+        dots
+        |> Seq.fold
+            (fun (X, Y) (x, y) -> Seq.max [X; x], Seq.max [Y; y])
+            (0, 0)
     let dotSet = Set.ofSeq dots
     let output =
         Array2D.init
@@ -48,7 +50,7 @@ let plot dots =
             (maxY + 1)
             (fun x y -> if Set.contains (x, y) dotSet then "██" else "  ")
     [0..maxY]
-    |> List.map (fun y -> String.Join("", output[*, y]))
+    |> List.map (fun y -> String.Join("", output.[*,y]))
     |> (fun rows -> String.Join("\n", rows))
 
 origami dots (Seq.take 1 folds)
