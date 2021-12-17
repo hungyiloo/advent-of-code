b96d75529680810199ff4536c748196e35d3970d
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-17 15:42:32 +0800

    2021 day 17... I don't like it

---
+++ 2021/17.fsx
@@ -0,0 +1,81 @@
+open System
+open System.IO
+
+let parseTarget (input: string) =
+  let input = input.Trim()
+  match input.Split([|": "|], StringSplitOptions.RemoveEmptyEntries) with
+  | [| "target area"; coords |] ->
+    match coords.Split([|", "|], StringSplitOptions.RemoveEmptyEntries) with
+    | [| xRange; yRange |] ->
+      let x1, x2 =
+        match xRange.Split([| "x="; ".." |], StringSplitOptions.RemoveEmptyEntries) with
+        | [| x1; x2 |] -> int x1, int x2
+        | _ -> failwith "invalid input"
+      let y1, y2 =
+        match yRange.Split([| "y="; ".." |], StringSplitOptions.RemoveEmptyEntries) with
+        | [| y1; y2 |] -> int y1, int y2
+        | _ -> failwith "invalid input"
+      x1, x2, y1, y2
+    | _ -> failwith "invalid input"
+  | _ -> failwith "invalid input"
+
+let inTarget target point =
+  let x, y = point
+  let x1, x2, y1, y2 = target
+  x1 <= x && x <= x2 && y1 <= y && y <= y2
+
+let overshoot target point =
+  let x, y = point
+  let _, x2, y1, _ = target
+  x > x2 || y < y1
+
+let step velocity point =
+  let vx, vy = velocity
+  let x, y = point
+  ((if vx > 0 then vx - 1 else 0), vy - 1),
+  (x + vx, y + vy)
+
+let simulate velocity target =
+  let checkTarget = inTarget target
+  let checkOvershoot = overshoot target
+  let rec search (path, velocity) =
+    let point = Seq.head path
+    if (checkTarget point) || (checkOvershoot point)
+    then path, velocity
+    else
+      let nextVelocity, nextPoint = step velocity point
+      search (nextPoint::path, nextVelocity)
+
+  let path,_  = search ([(0, 0)], velocity)
+
+  match path with
+  | point::_ when checkTarget point -> Some(velocity, path)
+  | _ -> None
+
+let inline (++) xs ys = xs |> List.collect (fun x -> ys |> List.map (fun y -> x, y))
+
+let triangularRoot n = round (((sqrt (((float n) * 8.0) + 1.0)) - 1.0) / 2.0) |> int
+
+let target =
+  // "target area: x=20..30, y=-10..-5"
+  File.ReadAllText "17.input.txt"
+  |> parseTarget
+
+// I really can't be bothered doing a mathematical analysis here to reduce the search space
+// So just putting in some best guesses for reasonable space to search for each part
+let _, x2, y1, _ = target
+
+[(triangularRoot x2)] ++ [y1..(abs y1)]
+|> List.choose
+  (fun (vx, vy) ->
+    match simulate (vx, vy) target with
+    | Some(_, path) -> Some (path |> Seq.maxBy snd)
+    | _ -> None)
+|> List.maxBy snd
+|> snd
+|> printfn "Part 1: %A"
+
+[0..x2] ++ [y1..(abs y1)]
+|> List.choose (fun (vx, vy) -> simulate (vx, vy) target)
+|> List.length
+|> printfn "Part 2: %A"
