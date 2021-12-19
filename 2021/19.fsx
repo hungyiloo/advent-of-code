4c8aa6034437b41d3039c90290e3cb536b6af19b
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-19 18:03:00 +0800

    2021 day 19... runs way too slowly

--- 2021/19.fsx
+++ 2021/19.fsx
@@ -37,20 +37,9 @@ let rotate rotation point =
     | 3 -> -1
     | _ -> failwith "Modulo failed?!"
 
-  let rotateX n (x, y, z) =
-    1*x + 0*y + 0*z,
-    0*x + (intcos n)*y - (intsin n)*z,
-    0*x + (intsin n)*y + (intcos n)*z
-
-  let rotateY n (x, y, z) =
-    (intcos n)*x + 0*y + (intsin n)*z,
-    0*x + 1*y - 0*z,
-    -(intsin n)*x + 0*y + (intcos n)*z
-
-  let rotateZ n (x, y, z) =
-    (intcos n)*x - (intsin n)*y + 0*z,
-    (intsin n)*x + (intcos n)*y - 0*z,
-    0*x + 0*y + 1*z
+  let rotateX n (x, y, z) = x, (intcos n)*y - (intsin n)*z, (intsin n)*y + (intcos n)*z
+  let rotateY n (x, y, z) = (intcos n)*x + (intsin n)*z, y, -(intsin n)*x + (intcos n)*z
+  let rotateZ n (x, y, z) = (intcos n)*x - (intsin n)*y, (intsin n)*x + (intcos n)*y, z
 
   let xr, yr, zr = rotation
 
@@ -62,22 +51,6 @@ let translate translation point =
   x + x', y + y', z + z'
 
 let intersect xs ys = xs |> List.filter (fun x -> List.contains x ys)
-
-let scanners =
-  Seq.append (File.ReadLines "19.input.txt") [""]
-  |> Seq.fold
-    (fun (scanners, points) curr ->
-    match curr with
-    | Split "," [ x; y; z ] -> scanners, (int x, int y, int z)::points
-    | "" ->
-     { found = List.isEmpty scanners
-       translation = 0,0,0
-       rotation = 0,0,0
-       beacons = points }::scanners, []
-    | _ -> scanners, points)
-    ([], [])
-  |> fst
-  |> List.rev
 
 let locate reference scanner =
   if scanner.found then
@@ -102,25 +75,94 @@ let locate reference scanner =
     result
 
 let solve scanners =
-  let mutable references, remaining = List.partition (fun s -> s.found) scanners
-  let mutable reference = List.head references
+  let mutable allFound, remaining = List.partition (fun s -> s.found) scanners
+  let mutable found = allFound
   while not (List.isEmpty remaining) do
-    let found, lost =
-      ((None, []), remaining)
-      ||> List.fold
-        (fun (found, lost) scanner ->
-         match found with
-         | Some _ -> found, scanner::lost
-         | None ->
-           match locate reference scanner with
-           | Some x -> Some x, lost
-           | None -> None, scanner::lost)
-    match found with
-    | Some x -> reference <- x
-    | _ -> ()
-    remaining <- lost
-    printfn "%A" (List.length remaining)
-  references
+    printfn "Still remaining: %A" (List.length remaining)
+    let mutable newlyFound = []
+    for reference in found do
+      let mutable stillRemaining = []
+      for scanner in remaining do
+        match locate reference scanner with
+        | Some x ->
+          printfn "Located %A" x
+          newlyFound <- x::newlyFound
+        | None -> stillRemaining <- scanner::stillRemaining
+      remaining <- stillRemaining
+    allFound <- allFound @ found
+    found <- newlyFound
+  allFound @ found
 
-solve scanners
-|> printfn "%A"
+let scanners =
+  Seq.append (File.ReadLines "19.input.txt") [""]
+  |> Seq.fold
+    (fun (scanners, points) curr ->
+    match curr with
+    | Split "," [ x; y; z ] -> scanners, (int x, int y, int z)::points
+    | "" ->
+     { found = List.isEmpty scanners
+       translation = 0,0,0
+       rotation = 0,0,0
+       beacons = points }::scanners, []
+    | _ -> scanners, points)
+    ([], [])
+  |> fst
+  |> List.rev
+
+// solve scanners
+// |> List.collect (fun s -> s.beacons)
+// |> List.distinct
+// |> List.length
+// |> printfn "Part 1: %A"
+
+let scannerLocations = [
+  (147, 155, -1150)
+  (84, -12, -2408)
+  (-1111, -15, -1172)
+  (-1069, 1334, -1335)
+  (140, 1336, -2439)
+  (152, 57, -3678)
+  (91, 1367, -3627)
+  (25, -1081, -3562)
+  (-1172, 1211, -2375)
+  (73, 2415, -2429)
+  (-1125, 2470, -1239)
+  (-1061, 3690, -1300)
+  (-2300, 2420, -1172)
+  (130, 2535, -3661)
+  (-2296, 1210, -2351)
+  (40, -2227, -3679)
+  (61, 1318, -4788)
+  (-1230, 1218, -4909)
+  (1189, 1325, -4821)
+  (-7, 2465, -4812)
+  (43, -2315, -2539)
+  (-1133, -2226, -3620)
+  (39, -3432, -3735)
+  (-1166, 2471, -3551)
+  (37, 3619, -1177)
+  (-1041, 3750, -2376)
+  (-1168, 4811, -2507)
+  (86, 4852, -1290)
+  (-2428, -2405, -3634)
+  (30, 2407, -5973)
+  (1176, 1237, -5981)
+  (1182, 2551, -6086)
+  (10, 4959, 1)
+  (51, 6081, -1202)
+  (139, 7342, -1144)
+  (-1201, 6159, -1249)
+  (4, 6024, -123)
+  (-1091, 4964, -52)
+  (-2393, 4828, -81)
+]
+
+let manhattanDistance (a, b) =
+  let x1, y1, z1 = a
+  let x2, y2, z2 = b
+  (abs (x1 - x2)) + (abs (y1 - y2)) + (abs (z1 - z2))
+
+scannerLocations ++ scannerLocations
+|> Seq.map manhattanDistance
+|> Seq.max
+|> printfn "Part 2: %A"
