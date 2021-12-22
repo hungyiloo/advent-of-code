7eb65e41932493db569b838e4aa49b172f47d7d4
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-22 20:50:07 +0800

    Tidy up day 22 solution

--- 2021/22.fsx
+++ 2021/22.fsx
@@ -1,11 +1,11 @@
 open System
 open System.IO
 
-type CubeState = | On | Off
-type Cube = | Cube of ((int64 * int64) * (int64 * int64) * (int64 * int64) * CubeState)
+type CuboidState = | On | Off
+type Cuboid = | Cuboid of (((int64 * int64) * (int64 * int64) * (int64 * int64)) * CuboidState)
 
-let overlap a b =
-  match a, b with
+let overlap rangeA rangeB =
+  match rangeA, rangeB with
   | (a1, a2), (b1, b2) when a1 <= b1 && b2 <= a2 -> Some (b1, b2)
   | (a1, a2), (b1, b2) when b1 <= a1 && a2 <= b2 -> Some (a1, a2)
   | (a1, a2), (b1, b2) when b1 <= a1 && a1 <= b2 && b2 <= a2 -> Some (a1, b2)
@@ -19,93 +19,67 @@ let overlap3d a b =
   | Some(cx), Some(cy), Some(cz) -> Some(cx, cy, cz)
   | _ -> None
 
-let volume cube =
-  match cube with
-  | Cube ((x1, x2), (y1, y2), (z1, z2), value) ->
+let volume cuboid =
+  match cuboid with
+  | Cuboid (points, value) ->
+    let (x1, x2), (y1, y2), (z1, z2) = points
     let sign =
       match value with
       | On -> 1L
       | Off -> -1L
     ((abs (x1 - x2)) + 1L) * ((abs (y1 - y2)) + 1L) * ((abs (z1 - z2)) + 1L) * sign
 
-let reduceCubes existingCubes cube =
-  existingCubes
+let reduceCuboids existingCuboids cuboid =
+  existingCuboids
   |> Seq.fold
-    (fun acc existingCube ->
-      match existingCube, cube with
-      | Cube(ax, ay, az, av), Cube(bx, by, bz, bv) ->
-        match (overlap3d (ax, ay, az) (bx, by, bz)), av, bv with
-        | Some(cx, cy, cz), On, _ -> Cube(cx, cy, cz, Off)::acc
-        | Some(cx, cy, cz), Off, _ -> Cube(cx, cy, cz, On)::acc
-        | None, _, _ -> acc)
-    existingCubes
-  |> (fun newCubes ->
-        match cube with
-        | Cube(_, _, _, On) -> cube::newCubes
-        | Cube(_, _, _, Off) -> newCubes)
+    (fun acc existingCuboid ->
+      match existingCuboid, cuboid with
+      | Cuboid(existingPoints, existingState), Cuboid(points, _) ->
+        match (overlap3d existingPoints points), existingState with
+        | Some overlap, On -> Cuboid(overlap, Off)::acc
+        | Some overlap, Off -> Cuboid(overlap, On)::acc
+        | None, _ -> acc)
+    existingCuboids
+  |> (fun newCuboids ->
+        match cuboid with
+        | Cuboid(_, On) -> cuboid::newCuboids
+        | Cuboid(_, Off) -> newCuboids)
 
-let (|SplitMulti|) (separators: string seq) (s: string) =
-  match s.Trim().Split(separators |> Seq.toArray, StringSplitOptions.RemoveEmptyEntries) with
-  | arr -> arr |> Seq.toList
+let countSwitchedOn cuboids =
+  cuboids
+  |> Seq.fold reduceCuboids []
+  |> Seq.map volume
+  |> Seq.sum
 
-let parseRange rangeString =
-  match rangeString with
-  | SplitMulti [ "="; ".." ] [ _; v1; v2 ] -> (int64 v1, int64 v2)
-  | e -> failwith (sprintf "Invalid range string %A" e)
-
-let inRange (min, max) x = min <= x && x <= max
-
-let ranges =
+let cuboids =
   File.ReadAllLines "22.input.txt"
   |> Seq.map
     (fun line ->
-      match line with
-      | SplitMulti [ " "; "," ] [ value; xRange; yRange; zRange ] ->
-        Cube
-          ( parseRange xRange,
-            parseRange yRange,
-            parseRange zRange,
+      let parseRange (rangeString: string) =
+        match rangeString.Split([| "="; ".." |], StringSplitOptions.RemoveEmptyEntries) with
+        | [| _; a; b |] -> (int64 a, int64 b)
+        | e -> failwith (sprintf "Invalid range string %A" e)
+
+      match line.Split([| " "; "," |], StringSplitOptions.RemoveEmptyEntries) with
+      | [| value; xRange; yRange; zRange |] ->
+        Cuboid
+          ( ( parseRange xRange,
+              parseRange yRange,
+              parseRange zRange ),
             if value = "on" then On else Off )
       | e -> failwith (sprintf "Invalid input format %A" e))
 
-ranges
+cuboids
   |> Seq.filter
-    (fun cube ->
-      let valid = inRange (-50L, 50L)
-      match cube with
-      | Cube((x1, x2), (y1, y2), (z1, z2), _) when valid x1 && valid x2 && valid y1 && valid y2 && valid z1 && valid z2 -> true
-      | _ -> false)
-  |> Seq.fold reduceCubes []
-  |> Seq.map volume
-  |> Seq.sum
+    (fun cuboid ->
+      let valid n = -50L <= n && n <= 50L
+      match cuboid with
+      | Cuboid(points, _) ->
+        let (x1, x2), (y1, y2), (z1, z2) = points
+        valid x1 && valid x2 && valid y1 && valid y2 && valid z1 && valid z2)
+  |> countSwitchedOn
   |> printfn "Part 1: %d"
 
-ranges
-  |> Seq.fold reduceCubes []
-  |> Seq.map volume
-  |> Seq.sum
+cuboids
+  |> countSwitchedOn
   |> printfn "Part 2: %d"
-
-// printfn "%A" (overlap3d ((-1L,2L), (-1L,2L), (0L,2L)) ((-2L,1L), (-2L,1L), (-2L,1L)))
-// printfn "%A" (volume (Cube((0L,2L), (0L,2L), (0L,2L), On)))
-// overlap (1,3) (4,6) |> printfn "%A should be None"
-// overlap (4,6) (1,3) |> printfn "%A should be None"
-// overlap (1,4) (4,6) |> printfn "%A should be None"
-// overlap (4,6) (1,4) |> printfn "%A should be None"
-// overlap (1,4) (0,2) |> printfn "%A should be (1, 2)"
-// overlap (0,2) (1,4) |> printfn "%A should be (1, 2)"
-// overlap (1,4) (1,3) |> printfn "%A should be (1, 3)"
-// overlap (1,3) (1,4) |> printfn "%A should be (1, 3)"
-// overlap (1,4) (2,3) |> printfn "%A should be (2, 3)"
-// overlap (2,3) (1,4) |> printfn "%A should be (2, 3)"
-// overlap (2,3) (1,4) |> printfn "%A should be (2, 3)"
-// overlap (1,4) (2,3) |> printfn "%A should be (2, 3)"
-// overlap (1,4) (2,4) |> printfn "%A should be (2, 4)"
-// overlap (2,4) (1,4) |> printfn "%A should be (2, 4)"
-// overlap (1,4) (3,6) |> printfn "%A should be (3, 4)"
-// overlap (3,6) (1,4) |> printfn "%A should be (3, 4)"
-// overlap (1,4) (4,6) |> printfn "%A should be None"
-// overlap (4,6) (1,4) |> printfn "%A should be None"
-// overlap (1,4) (5,6) |> printfn "%A should be None"
-// overlap (5,6) (1,4) |> printfn "%A should be None"
-
