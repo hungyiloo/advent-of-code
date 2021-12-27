4da3853d5693f599d19318e730d0d74ef2f9d27b
Hung-Yi Loo <hungyi.loo@gmail.com>
2021-12-27 22:16:38 +0800

    Finish 2020 day 13, finally

--- 2020/13.ts
+++ 2020/13.ts
@@ -1,28 +1,41 @@
-import { getLines, pop } from "../lib/streams.ts";
+import { filter, map, reduce, split } from "../lib/array.ts";
+import { pipe } from "../lib/pipe.ts";
+import { getLines, pop$ } from "../lib/streams.ts";
 
 const inputData$ = getLines("13.input.txt");
-const timestampRaw = await pop(inputData$)
-const scheduleRaw = await pop(inputData$)
-// const scheduleRaw = "17,x,13,19";
+const timestampRaw = await pop$(inputData$);
+const scheduleRaw = await pop$(inputData$);
 
 if (!timestampRaw || !scheduleRaw) Deno.exit();
 
 const timestamp = parseInt(timestampRaw);
 
-const schedule = scheduleRaw
-  .split(",")
-  .map((x) => x === "x" ? null : parseInt(x, 10));
+const schedule = pipe(
+  scheduleRaw,
+  split(","),
+  map((x) => x === "x" ? 1 : parseInt(x, 10)),
+);
 
-const busIds = schedule.filter((x) => x !== null) as number[];
-
-const getBusWaitingTimesAt = (timestamp: number) =>
-  busIds.map((busId) => ({
+pipe(
+  schedule,
+  filter((x) => x !== 1),
+  map((busId) => ({
     busId,
     nextBusIn: (busId - (timestamp % busId)) % busId,
-  }));
+  })),
+  reduce(
+    (min, curr) => curr.nextBusIn < min.nextBusIn ? curr : min,
+    { nextBusIn: Infinity, busId: 0 },
+  ),
+  (nextBus) => console.log("Part 1:", nextBus.busId * nextBus.nextBusIn),
+);
 
-const nextBusAtTimestamp = getBusWaitingTimesAt(timestamp)
-  .reduce((min, curr) => curr.nextBusIn < min.nextBusIn ? curr : min);
-
-console.log("Part 1:", nextBusAtTimestamp.busId * nextBusAtTimestamp.nextBusIn);
-
+pipe(
+  schedule,
+  map((busId, position) => ({ busId, position })),
+  reduce(({ t, period }, { busId, position }) => {
+    while ((t + position) % busId !== 0) t += period;
+    return { t, period: period * busId };
+  }, { t: 0, period: 1 }),
+  ({ t }) => console.log("Part 2:", t),
+);
