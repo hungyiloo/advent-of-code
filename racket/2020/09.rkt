4250b43f777001701d44fcaad129f175c07d5e9b
Hung-Yi Loo <hungyi.loo@gmail.com>
2022-08-08 22:53:09 +0800

    Rewrite 2020 day 9 in racket "for" comprehensions

--- racket/2020/09.rkt
+++ racket/2020/09.rkt
@@ -3,45 +3,44 @@
 (require "../lib/parsing.rkt")
 (require "../lib/lists.rkt")
 
-(: nums (Listof Integer))
 (define nums
   (~> (file->lines "../../input/2020/09.txt")
       (map string->exact-integer _)))
 
-(define (sum [xs : (Listof Integer)]) (apply + xs))
-
-(define part1 0)
-
 (define interval 25)
 
-(define combos (combinations (take nums interval) 2))
+(define (sum [xs : (Listof Integer)]) (apply + xs))
 
-(for ([num (drop nums interval)]
-      [prev-num nums]
-      #:final (not (member num (map sum combos))))
-  (set! part1 num)
-  (set! combos
-        (map
-         (lambda ([pair : (Listof Integer)])
-           (match pair
-             [(list a b) #:when (= a prev-num) (list b num)]
-             [else pair]))
-         combos)))
+(define part1
+  (for/fold ([answer : Integer 0]
+             [combos : (Listof (Listof Integer)) (combinations (take nums interval) 2)]
+             #:result answer)
+            ([num (drop nums interval)]
+             [prev-num nums]
+             #:final (not (member num (map sum combos))))
+    (values num
+            (map
+             (lambda ([pair : (Listof Integer)])
+               (match pair
+                 [(list a b) #:when (= a prev-num) (list b num)]
+                 [else pair]))
+             combos))))
 
 (displayln (format "Part 1: ~a" part1))
 
-(: part2 (U False Integer))
-(define part2 #f)
-
-(for ([i (range (length nums))]
-      #:break part2)
-  (define subtotal 0)
-  (for ([j (range (+ i 1) (length nums))]
-        #:break (or part2 (> subtotal part1)))
-    (define subset (slice nums i j))
-    (set! subtotal (sum subset))
-    (when (= subtotal part1)
-      (set! part2 (+ (apply min subset) (apply max subset))))))
+(define part2
+  (for/or ([i (range (length nums))])
+    : (U False Integer)
+    (for/fold ([subtotal : Integer 0]
+               [part2 : (U False Integer) #f]
+               #:result part2)
+              ([j (range (+ i 1) (length nums))]
+               #:break (or part2 (> subtotal part1)))
+      (define subset (slice nums i j))
+      (values (sum subset)
+              (if (= subtotal part1)
+                  (+ (apply min subset) (apply max subset))
+                  part2)))))
 
 (match part2
   [(? number?)
