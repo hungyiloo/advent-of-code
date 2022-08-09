4b9af126b49197328f0d7dec2751a348879c49a5
Hung-Yi Loo <hungyi.loo@gmail.com>
2022-08-09 20:49:21 +0800

    Rewrite 2020 day 9 part 2 with recursion

--- racket/2020/09.rkt
+++ racket/2020/09.rkt
@@ -1,11 +1,7 @@
 #lang typed/racket
-(require threading)
 (require "../lib/parsing.rkt")
-(require "../lib/lists.rkt")
 
-(define nums
-  (~> (file->lines "../../input/2020/09.txt")
-      (map string->exact-integer _)))
+(define nums (file->lines->integers "../../input/2020/09.txt"))
 
 (define interval 25)
 
@@ -13,34 +9,36 @@
 
 (define part1
   (for/fold ([answer : Integer 0]
-             [combos : (Listof (Listof Integer))
+             [pairs : (Listof (Listof Integer))
                      (combinations (take nums interval) 2)]
              #:result answer)
             ([num (drop nums interval)]
              [prev-num nums]
-             #:final (not (member num (map sum combos))))
+             #:final (not (member num (map sum pairs))))
     (values num
-            (for/list ([combo combos])
-              (match combo
+            (for/list ([pair pairs])
+              (match pair
                  [(list a b) #:when (= a prev-num) (list b num)]
-                 [else combo])))))
+                 [else pair])))))
 
 (displayln (format "Part 1: ~a" part1))
 
-(define part2
-  (for/or ([i (range (length nums))])
-    : (U False Integer)
-    (for/fold ([subtotal : Integer 0]
-               [part2 : (U False Integer) #f]
-               #:result part2)
-              ([j (range (+ i 1) (length nums))]
-               #:break (or part2 (> subtotal part1)))
-      (define subset (slice nums i j))
-      (values (sum subset)
-              (if (= subtotal part1)
-                  (+ (apply min subset) (apply max subset))
-                  part2)))))
+(define (part2)
+  (: search-inside (-> (Listof Integer) (Listof Integer) (U False Integer)))
+  (define (search-inside xs ys)
+    (define subtotal (sum xs))
+    (cond
+      [(= subtotal part1) (+ (apply min xs) (apply max xs))]
+      [(> subtotal part1) #f]
+      [(empty? ys) #f]
+      [else (search-inside (cons (car ys) xs) (cdr ys))]))
 
-(match part2
-  [(? number?)
-   (displayln (format "Part 2: ~a" part2))])
+  (: search-up (-> (Listof Integer) (U False Integer)))
+  (define (search-up [xs : (Listof Integer)])
+    (or (search-inside '() xs)
+        (search-up (cdr xs))))
+
+  (search-up nums))
+
+(match (part2)
+  [(? number? answer) (displayln (format "Part 2: ~a" answer))])
