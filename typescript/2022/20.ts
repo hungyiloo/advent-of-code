024ed04f176e09aabd1bce75176b686da9f92a61
Hung-Yi Loo <hungyi.loo@gmail.com>
2023-07-09 20:56:47 +0800

    2022 Day 20 part 2

--- typescript/2022/20.ts
+++ typescript/2022/20.ts
@@ -1,38 +1,31 @@
 const puzzleInput = (await Deno.readTextFile("../../input/2022/20.txt")).trim();
 
-function parse(input: string) {
-  return input.split("\n").map(n => ({ n: Number(n), moved: false }));
+function parse(input: string, multiplier: number) {
+  return input.split("\n").map((n) => ({ n: Number(n) * multiplier }));
 }
 
-const nums = parse(puzzleInput);
-
-let moves = 0;
-let index = 0;
-// console.log(nums.map(({ n }, i) => i === index ? `[${n}]` : n).join(', '))
-while (moves < nums.length) {
-  const { n, moved } = nums[index]
-  if (!moved) {
-    // console.log(nums.map(({ n }, i) => i === index ? `[${n}]` : n).join(', '))
-    nums.splice(index, 1)
-    const target = (index + n + nums.length) % nums.length
-    // console.log(`Moving ${n} to position ${target}`)
-    nums.splice(target, 0, { n, moved: true })
-    moves++;
-    // console.log(nums.map(({ n }) => n).join(', '))
-  } else {
-    index = (index + 1) % nums.length;
-    // console.log(nums.map(({ n }, i) => i === index ? `[${n}]` : n).join(', '))
+function mix(repeats = 1, decryptionKey = 1) {
+  const nums = parse(puzzleInput, decryptionKey);
+  const queue = [...nums];
+  for (let ii = 0; ii < repeats; ii++) {
+    for (const item of queue) {
+      const index = nums.indexOf(item);
+      nums.splice(index, 1);
+      const destination = (index + item.n + nums.length) % nums.length;
+      nums.splice(destination, 0, item);
+    }
   }
-  // console.log('\n')
-}
-// console.log(nums.map(({ n }, i) => i === index ? `[${n}]` : n).join(', '))
 
-const zeroPosition = nums.findIndex(({ n }) => n === 0)
-const a = nums[(zeroPosition + 1000) % nums.length].n
-const b = nums[(zeroPosition + 2000) % nums.length].n
-const c = nums[(zeroPosition + 3000) % nums.length].n
-const part1 = a + b + c;
+  const zeroPosition = nums.findIndex(({ n }) => n === 0);
+  const a = nums[(zeroPosition + 1000) % nums.length].n;
+  const b = nums[(zeroPosition + 2000) % nums.length].n;
+  const c = nums[(zeroPosition + 3000) % nums.length].n;
+
+  return a + b + c;
+}
+
+const part1 = mix();
 console.log("Part 1:", part1);
 
-const part2 = 0;
+const part2 = mix(10, 811589153);
 console.log("Part 2:", part2);
