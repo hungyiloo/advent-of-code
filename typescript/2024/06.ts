09a506418fb57cd1ba7b1b250276d3c2648ce698
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-07 19:22:40 +0700

    feat(2024-06): massive optimisation of naive solution

--- typescript/2024/06.ts
+++ typescript/2024/06.ts
@@ -15,12 +15,6 @@ type Entity = {
   type: EntityType;
   position: [number, number]
 };
-const entities = grid.flatMap((line, row) =>
-  line.map((cell, col) => ({ type: cell, position: [row, col] }) as Entity),
-);
-const guard = entities.find((e) => e.type === EntityType.Guard)!;
-const obstacles = entities.filter((e) => e.type === EntityType.Obstacle);
-const spaces = entities.filter((e) => e.type === EntityType.Empty);
 
 enum Direction { Left, Right, Up, Down }
 
@@ -40,22 +34,26 @@ function turn(direction: Direction) {
   }
 }
 
-function step(pos: [number, number], vector: [number, number]): 'exit' | 'blocked' | [number, number] {
+function step(
+  pos: [number, number],
+  direction: Direction,
+  blocks: [number,number][],
+): 'exit' | 'blocked' | [number, number] {
+  const vector = DirectionVectors[direction]
   const row = pos[0] + vector[0]
   const col = pos[1] + vector[1]
   if (row < 0 || row > gridMaxRow || col < 0 || col > gridMaxCol) {
     return 'exit'
-  } else if (obstacles.find(o => o.position[0] === row && o.position[1] === col)) {
+  } else if (blocks.find(b => b[0] === row && b[1] === col)) {
     return 'blocked'
   } else {
     return [row, col]
   }
 }
 
-function simulate() {
-  let pos = guard.position
+function simulate(pos: [number, number], blocks: [number, number][]) {
   let direction = Direction.Up
-  let nextPos = step(pos, DirectionVectors[direction])
+  let nextPos = step(pos, direction, blocks)
   let loop = false
   const visited = new Set([String(pos)])
   const blockedAt = new Set<string>()
@@ -75,13 +73,11 @@ function simulate() {
       pos = nextPos
       visited.add(posKey)
     }
-    nextPos = step(pos, DirectionVectors[direction])
+    nextPos = step(pos, direction, blocks)
   }
 
   return { visited, loop }
 }
-
-console.log("Part 1:", simulate().visited.size)
 
 function rewritableLog(message?: string, maxLength = 25) { 
   Deno.stdout.writeSync(new TextEncoder().encode(new Array(maxLength).fill('\b').join(''))) 
@@ -90,17 +86,30 @@ function rewritableLog(message?: string, maxLength = 25) {
   }
 }
 
+const entities = grid.flatMap((line, row) =>
+  line.map((cell, col) => ({ type: cell, position: [row, col] }) as Entity),
+);
+const guard = entities.find((e) => e.type === EntityType.Guard)!.position;
+const obstacles = entities.filter((e) => e.type === EntityType.Obstacle).map(o => o.position);
+const visited = simulate(guard, obstacles).visited
+console.log("Part 1:", visited.size)
+
+const potentialObstacles = entities
+  .filter((e) => e.type === EntityType.Empty)
+  .map(s => s.position)
+  // Only positions visited in Part 1 need to be considered.
+  // Why? Putting an obstacle where the guard never would have walked makes no difference!
+  .filter(p => visited.has(String(p))); 
+
 rewritableLog("Searching for loops...")
 let loopCount = 0
-for (const space of spaces) {
-  obstacles.push({ type: EntityType.Obstacle, position: space.position })
-  if (simulate().loop) { 
+for (const potentialObstacle of potentialObstacles) {
+  if (simulate(guard, obstacles.concat([potentialObstacle])).loop) { 
     loopCount++
     rewritableLog(`Searching... (${loopCount})`)
   }
-  obstacles.pop()
 }
-
 rewritableLog(new Array(25).fill(' ').join(''))
 rewritableLog()
+
 console.log("Part 2:", loopCount)
