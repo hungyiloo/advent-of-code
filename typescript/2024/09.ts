7624788dc91846ccd52c7da6ffb3950ff1c53785
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-12 18:23:07 +0700

    refactor(2024-09): use same logic for both parts + comments

--- typescript/2024/09.ts
+++ typescript/2024/09.ts
@@ -5,38 +5,41 @@ const puzzleInput = (await Deno.readTextFile("../../input/2024/09.txt")).trim();
 const parse = sss.array('', Number)
 const diskMap = parse(puzzleInput).map((length, ii) => ({ id: ii % 2 ? null : ii/2, length }))
 
-function clone<T>(o: T): T { return JSON.parse(JSON.stringify(o)) }
+function deepClone<T>(o: T): T { return JSON.parse(JSON.stringify(o)) }
 
-function compress(disk: typeof diskMap) {
-  disk = clone(disk)
-  return disk.reduce(
-    (acc, curr, ii) => {
-      if (curr.id !== null && curr.length > 0) {
-        acc.push(curr)
+// NOTE: The solution would be faster with dedicated implementations for each part,
+//       but it's much more satisfying to have the same code work for both parts.
+function compress(disk: typeof diskMap, defrag: boolean) {
+  disk = deepClone(disk)
+
+  for (const file of disk.filter(s => s.id !== null).reverse()) {
+    while (file.id) {
+      // Find the first free space that fits
+      const spaceIndex = disk.findIndex(s => s.id === null && (!defrag || s.length >= file.length))
+
+      // If there's no suitable space, skip this file.
+      // Also abort if the free space is not to the left of the file.
+      if (spaceIndex === -1 || spaceIndex >= disk.indexOf(file)) break;
+
+      // Move the file
+      const space = disk[spaceIndex]
+      if (space.length === file.length) {
+        space.id = file.id
+        file.id = null
+      } else if (space.length < file.length) {
+        space.id = file.id
+        file.length -= space.length
+        // commented out for speed -- while technically correct, not necessary for answers
+        // disk.splice(disk.indexOf(file) + 1, 0, { id: null, length: space.length })
       } else {
-        let freeSpace = curr.length
-        while (freeSpace > 0) {
-          const lastFileIndex = disk.findLastIndex(f => f.id !== null)!
-          if (lastFileIndex <= ii) break;
-          const lastFile = disk[lastFileIndex]
-          if (freeSpace >= lastFile.length) {
-            acc.push({ ...lastFile })
-            freeSpace -= lastFile.length
-            disk.length = lastFileIndex
-          } else {
-            acc.push({ ...lastFile, length: freeSpace })
-            lastFile.length -= freeSpace
-            if (lastFile.length === 0) {
-              disk.length = lastFileIndex
-            }
-            freeSpace = 0
-          }
-        }
+        space.length -= file.length
+        disk.splice(spaceIndex, 0, { ...file })
+        file.id = null
       }
-      return acc
-    },
-    [] as typeof disk
-  )
+    }
+  }
+
+  return disk
 }
 
 function checksum(disk: typeof diskMap) {
@@ -44,44 +47,13 @@ function checksum(disk: typeof diskMap) {
   let position = 0;
   for (const segment of disk) {
     if (segment.id !== null) {
-      const sumOfRange = (segment.length/2 * (2*position + segment.length - 1)) 
-      result += sumOfRange * segment.id
+      // adapted formula for sum of a range of integers (S = n(a + l)/2)
+      result += segment.length * (2*position + segment.length - 1) / 2 * segment.id 
     }
     position += segment.length
   }
   return result
 }
 
-console.log("Part 1:", checksum(compress(diskMap)))
-
-function compressDefrag(disk: typeof diskMap) {
-  disk = clone(disk)
-
-  // Iterate through files in reverse
-  for (const file of disk.filter(s => s.id !== null).reverse()) {
-    // Find the position of the file on the current disk state
-    const fileIndex = disk.indexOf(file)
-
-    // so that we can find the first free space that fits
-    const spaceIndex = disk
-      .slice(0, fileIndex) // but only if its left of the file
-      .findIndex(s => s.id === null && s.length >= file.length)
-
-    // and if there's no big enough space, skip this file
-    if (spaceIndex === -1) continue
-
-    // move the file
-    const space = disk[spaceIndex]
-    if (space.length === file.length) {
-      space.id = file.id
-    } else {
-      // and reallocate any leftover free space, if space > file
-      disk.splice(spaceIndex, 1, { ...file }, { id: null , length: space.length - file.length })
-    }
-    file.id = null
-  }
-
-  return disk
-}
-
-console.log("Part 2:", checksum(compressDefrag(diskMap)))
+console.log("Part 1:", checksum(compress(diskMap, false)))
+console.log("Part 2:", checksum(compress(diskMap, true)))
