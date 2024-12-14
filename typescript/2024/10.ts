5595fc494442ad571dda1377fa2188e08e03752f
Hung-Yi Loo <hungyi.loo@gmail.com>
2024-12-14 12:56:20 +0700

    refactor(2024-10): simplify

--- typescript/2024/10.ts
+++ typescript/2024/10.ts
@@ -7,41 +7,36 @@ const grid = parse(puzzleInput)
 const gridMaxRow = grid.length - 1
 const gridMaxCol = grid[0].length - 1
 
-function walk(row: number, col: number, allPossibleTrails = false): number {
-  if (grid[row][col] !== 0) return 0
+function walk(start: [number, number]) {
+  if (grid[start[0]][start[1]] !== 0) return { trails: 0, summits: 0 }
 
-  const summits = new Set<string>()
+  const summited = new Set<string>()
 
-  function inner(row: number, col: number) {
+  function step(row: number, col: number) {
     const height = grid[row][col]
     if (height === 9) {
-      summits.add(String([row, col]))
+      summited.add(`${row},${col}`)
       return 1
     } else {
       const oneLevelHigher = height + 1
       let trails = 0
-      if (grid[row+1]?.[col] === oneLevelHigher) trails += inner(row+1, col)
-      if (grid[row-1]?.[col] === oneLevelHigher) trails += inner(row-1, col)
-      if (grid[row]?.[col+1] === oneLevelHigher) trails += inner(row, col+1)
-      if (grid[row]?.[col-1] === oneLevelHigher) trails += inner(row, col-1)
+      if (grid[row+1]?.[col] === oneLevelHigher) trails += step(row+1, col)
+      if (grid[row-1]?.[col] === oneLevelHigher) trails += step(row-1, col)
+      if (grid[row]?.[col+1] === oneLevelHigher) trails += step(row, col+1)
+      if (grid[row]?.[col-1] === oneLevelHigher) trails += step(row, col-1)
       return trails
     }
   }
 
-  const trails = inner(row, col)
-  return allPossibleTrails ? trails : summits.size
+  const trails = step(...start)
+  return { trails, summits: summited.size }
 }
 
-let part1 = 0
-for (let row = 0; row <= gridMaxRow; row++) 
-  for (let col = 0; col <= gridMaxCol; col++) 
-    part1 += walk(row, col)
+function* allPoints() { 
+  for (let row = 0; row <= gridMaxRow; row++) 
+    for (let col = 0; col <= gridMaxCol; col++) 
+      yield [row, col] as [number, number]
+}
 
-console.log("Part 1:", part1)
-
-let part2 = 0
-for (let row = 0; row <= gridMaxRow; row++) 
-  for (let col = 0; col <= gridMaxCol; col++) 
-    part2 += walk(row, col, true)
-
-console.log("Part 2:", part2)
+console.log("Part 1:", allPoints().reduce((s, p) => s + walk(p).summits, 0))
+console.log("Part 2:", allPoints().reduce((s, p) => s + walk(p).trails, 0))
